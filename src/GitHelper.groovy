da9de39f0702f983fc3f71fedd60c95f62eae2d8
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-02-03 17:09:03 +0300

    Commit message

---
+++ src/GitHelper.groovy
@@ -0,0 +1,23 @@
+class GitHelper {
+
+	private final Script
+
+		GitHelper(Script script){
+			this.script = script
+		}
+		
+
+	List getBranches(){
+		
+		String remoteBranches = script.bat(returnStdout : true, script : '@echo off | git branch -a | findstr \"remotes/origin\"')
+		remoteBranches = remoteBranches.replace("renites/origin/", "")
+		
+		List result = []
+		
+		remoteBranches.readLines().each { line ->
+			result.add(line.trim())
+		}
+		
+		return result
+	}
+}
--- vars/jobGenerator.groovy
+++ vars/jobGenerator.groovy
@@ -1,3 +1,27 @@
+import GitHelper
+
 def call(body) {
+	
+	inputParams = [:]
+	
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = inputParams
+	body()
+	
 	echo 'job generator from demo library'
+	
+	
+	
+	node {
+		stage('Pull Source'){
+			checkout scm: [$class: 'GitSCM',
+									   userRemoteConfigs: [[url: inputParams.repoUrl]]
+						]
+		}
+		
+		stage('Update Module Jobs'){
+			List branches = new GitHelper(this).getBranches()
+			echo branches
+		}
+	}
 }
